a0aecaaf6c957210faade049e4c296fcf72ed4ba
Mark D. Rufino <mark.rufino@coreproc.ph>
2018-05-26 12:52:27 +0800

    Initial commit

---
+++ ToiletBook/ToiletBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToiletBook
+//
+//  Created by Mark D. Rufino on 05/26/2018.
+//  Copyright © 2018 Toilet Book Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
